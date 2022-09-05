9b01512fcab48fee8e50c395755c36ac7da17845
vinay-kumar-chowdary <112849736+vinay-kumar-chowdary@users.noreply.github.com>
2022-09-05 14:02:43 +0530

    Add files via upload

---
+++ area of the circle.py
@@ -0,0 +1,3 @@
+r=float(input('enter the radius of the circle:'))
+b=float(3.14*r*r)
+print('area of the circle is %0.1f '%(b))
---
+++ simple interest.py
@@ -0,0 +1,5 @@
+p=float(input("enter the principle amount"))
+t=float(input("enter the time"))
+r=float(input("enter the rate of interest"))
+a=(p*t*r)/100
+print("simple interest ",(a))
